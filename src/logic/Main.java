e7482545c31724df9dc72e346ec6038d2c64d9fc
Lucas Emiliano Saleme <75321699+Lucases27@users.noreply.github.com>
2021-09-14 16:35:37 -0300

    Initial commit

---
+++ src/logic/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logic;
+
+import gui.Jframe;
+
+/**
+ *
+ * @author lucas
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+        Jframe jframe = new Jframe();
+        jframe.setVisible(true);
+        jframe.setLocationRelativeTo(null);
+    }
+}
